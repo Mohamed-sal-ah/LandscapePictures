27c0b530365c237fd5c181e0e20c4d8d68252391
Mohamed Salah Ahmed <mohamed.salahahmed@student.kyh.se>
2021-05-31 10:53:29 +0200

    added firebase configuration file

---
+++ src/firebase/configKeys.ts
@@ -0,0 +1,10 @@
+// Your web app's Firebase configuration
+export const config = {
+  apiKey: "API-KEY",
+  authDomain: "AUTH-DOMAIN",
+  databaseURL: "DATABASE-URL",
+  projectId: "PROJECT-ID",
+  storageBucket: "STORAGE-BUCKET",
+  messagingSenderId: "MESSAGING-SENDER-ID",
+  appId: "APP-ID"
+};
